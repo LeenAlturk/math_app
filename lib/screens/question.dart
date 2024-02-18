ad4bd6dbb441a192e4ca82621a6a141513cdd30e
leen <zizozizoalturk@gmail.com>
2024-02-18 22:43:36 +0300

    cardswiper

--- lib/screens/question.dart
+++ lib/screens/question.dart
@@ -1,3 +1,17 @@
 import 'package:card_swiper/card_swiper.dart';
 
 import 'package:flutter/material.dart';
+
+class CardSwiper extends StatefulWidget {
+  const CardSwiper({super.key});
+
+  @override
+  State<CardSwiper> createState() => _CardSwiperState();
+}
+
+class _CardSwiperState extends State<CardSwiper> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
